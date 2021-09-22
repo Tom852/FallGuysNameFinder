5454c1fb95908010538fa74653a478d3bf27556a
Tom K <38071521+Tom852@users.noreply.github.com>
2021-09-22 15:34:03 +0200

    add unhandled exception handler

--- FallGuysNameFinder/App.xaml.cs
+++ FallGuysNameFinder/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Serilog;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace FallGuysNameFinder
 {
@@ -7,5 +9,15 @@ namespace FallGuysNameFinder
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+        }
+
+        void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            Log.Fatal(e.Exception, "An unhandled exception occurred");
+            MessageBox.Show("An unhandled exception occurred: " + e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
